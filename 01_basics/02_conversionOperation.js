972d4299774699a847913ff15c4a53486192181f
Aniket Singh <129079574+aniketsingh1023@users.noreply.github.com>
2025-04-04 12:48:20 +0000

    converstion ans operations

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,19 @@
+let score  = "1"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+
+let isLoggedIn = "Aniket"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//0 - false (when empty string is passed )
+// 1 - true (when non empty string is passed) ie string with characters present
+
+
+
+ 
