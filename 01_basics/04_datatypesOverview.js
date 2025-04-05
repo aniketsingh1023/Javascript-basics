2903317633d4dbc2b889583b0c3eebd8b9b9f5a4
Aniket Singh <129079574+aniketsingh1023@users.noreply.github.com>
2025-04-05 21:31:37 +0000

    all datatypes

---
+++ 01_basics/04_datatypesOverview.js
@@ -0,0 +1,35 @@
+// Primitive 
+
+//7 types : String , Number , Boolean , null , undefined , Symbol , BigInt
+
+const score = 100
+const scoreValues = 100.3
+  //no separatre float defined in js , both are number
+    
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+ 
+console.log(id === anotherId);
+
+const bigNumber = 8467589769744598745n
+
+//Reference(Non primitive)
+
+//Array, Objects, Functions 
+
+const heros = ["shaktiman" , " naagraj" , "doga"]
+let myObject = {
+    name: "Aniket",
+    age: 22,
+}
+const myFunction = function(){
+    console.log("Hello World");
+
+}
+
+//datatype pta krna
+console.log(typeof bigNumber);
