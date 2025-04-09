c8101339f9db46473628909166360651e4cb719a
Aniket Singh <129079574+aniketsingh1023@users.noreply.github.com>
2025-04-09 11:41:00 +0000

    numbers and maths

--- 01_basics/05_numsAndMaths.js
+++ 01_basics/05_numsAndMaths.js
@@ -6,4 +6,42 @@ const balance = new Number(100)
 console.log(balance);
 
 
-console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.8949
+
+console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString());
+//toLocaleString comma sparated numbers return krta hai , default us standards use krke
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//+++++++++++++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++
+console.log(Math);
+ 
+//Math.abs() --> absolute value
+//Math.round() --> round of 
+//ceiling and floor for round off --> Math.ceil() , Math.floor()
+//Math.min() , Math.max(). --. min and max value in an array 
+
+console.log(Math.random()*10);
+// gives random values between 0 and 1 
+
+
+// but this gives a random value , but 
+// what if we need those values between a certain range 
+
+// lets say between 10 and 20 
+// there is a trick using min max and floor , here involved 
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+ 
