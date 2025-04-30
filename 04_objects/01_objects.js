ae7c5ce27e54966e79492688968def8137a0f4a2
Aniket Singh <129079574+aniketsingh1023@users.noreply.github.com>
2025-04-30 11:58:33 +0000

    added objects

---
+++ 04_objects/01_objects.js
@@ -0,0 +1,98 @@
+//singleton is made when we declare objects as a constructor(object.create)
+//objects can be declared in 2 ways - literals , constructor
+
+
+
+//objects literals
+//objects conatain key/value pairs
+const mySym = Symbol("key1")
+const JsUser = {
+    name : "Aniket",
+    age: 20,
+    location: "Indore",
+    email: "aniket@apple.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
